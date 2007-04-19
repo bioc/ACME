6d8bdca49d1959cc3d47683a703e0c309431ac54
Sean Davis <sdavis2@mail.nih.gov>
2007-04-19 22:06:30 +0000

    1.1.11 ====== * Fixed bug in write.sgr when colnames were not defined on vals
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ACME@24047 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/write.sgr.R
+++ R/write.sgr.R
@@ -2,17 +2,20 @@ write.sgr <- function(agff,raw=TRUE,vals=TRUE,directory='.') {
   if (!(class(agff) %in% c('aGFF','aGFFCalc'))) {
     stop('Need agff to be an aGFF or aGFFCalc object')
   }
+  sampnames <- ifelse(is.null(colnames(agff@data)),
+                      paste('sample',1:ncol(agff@data)),
+                      colnames(agff@data))
   for(i in 1:ncol(agff@data)) {
     if (class(agff)=='aGFFCalc'){
       if(vals) {
-        filename <- file.path(directory,sprintf("%s_thresh%3.2f.sgr",colnames(agff@vals)[i],agff@threshold))
+        filename <- file.path(directory,sprintf("%s_thresh%3.2f.sgr",sampnames[i],agff@threshold))
         cat(filename,"\n")
         write.table(data.frame(agff@annotation[,c('Chromosome','Location')],-log10(agff@vals[,i]+min(agff@vals[agff@vals[,i]>0,i]))),file=filename,
                     sep="\t",col.names=FALSE,row.names=FALSE,quote=FALSE)
       }
     }
     if(raw) {
-      filename <- file.path(directory,sprintf("%s_raw.sgr",colnames(agff@data)[i]))
+      filename <- file.path(directory,sprintf("%s_raw.sgr",sampnames[i]))
       cat(filename,"\n")
       write.table(data.frame(agff@annotation[,c('Chromosome','Location')],agff@data[,i]),file=filename,
                   sep="\t",col.names=FALSE,row.names=FALSE,quote=FALSE)
